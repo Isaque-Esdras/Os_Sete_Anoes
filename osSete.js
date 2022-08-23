2e284e59278b9ada797d78e86935d9c9f01b5db0
Isaque-Esdras <108592138+Isaque-Esdras@users.noreply.github.com>
2022-08-23 16:45:07 -0300

    new commit

---
+++ osSete.js
@@ -0,0 +1,33 @@
+var inpute = document.getElementById('chave')
+var valorInpute = Number(inpute.value)
+var aImagem = document.getElementById('imagemInicial')
+var lista = [8, 12, 15, 18]
+
+function encontrar() {
+    if (proNumero(inpute.value) && !praLista(inpute.value, lista)) {
+        aImagem.src = './anoes.jpg'
+        window.alert("Parabéns, você achou os sete.")
+    }else{
+        aImagem.src = './floresta.jpg'
+        window.alert("Opsss, não foi dessa vez.")
+    }
+}
+
+
+
+function proNumero(n) {
+    if (Number(n) >=8 && Number(n) <=20) {
+        return true
+    }else{
+        return false
+    }
+}
+
+
+function praLista (n,a) {
+    if (a.indexOf(Number(n))!= -1) {
+        return true
+    }else{
+        return false
+    }
+}
